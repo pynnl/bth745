abfb558c78f5ef15921571b578c0800c4719adf9
Jay Yin <jayyin11043@hotmail.com>
2019-11-07 20:21:14 -0500

    added some time stamps

--- src/components/TimeTable.vue
+++ src/components/TimeTable.vue
@@ -1,11 +1,38 @@
 <template>
-  <v-calendar
+  <v-calendar style="margin: auto;min-width: 1000px; width: 50%; height: 50% "
     type="week"
     :max-days="5"
     :weekdays=[1,2,3,4,5]
     :first-interval=8
     :interval-count=14
     :interval-height=55
+    :events="courses"
+    :event-name="courses.code"
+    :now="today"
+    :value="today"
   >
   </v-calendar>
 </template>
+
+<script>
+import courses from './courses'
+
+export default {
+  data: () => ({
+    today: '2019-01-08',
+    courses,
+    events: [
+      {
+        name: 'BTH 745',
+        start: '2019-01-07 09:00',
+        end: '2019-01-07 10:00'
+      },
+      {
+        name: 'DPS 924',
+        start: '2019-01-08 10:30',
+        end: '2019-01-08 15:30'
+      }
+    ]
+  })
+}
+</script>
--- src/components/courses.js
+++ src/components/courses.js
@@ -1,6 +1,6 @@
 export default [
   // TODO: add room & time for a course
-  { code: 'BTH 745', name: 'Human-Computer Interaction', section: 'NBB' },
-  { code: 'DPS 924', name: 'Mobile App Development - Android', section: 'NSA' }
+  { code: 'BTH 745', name: 'Human-Computer Interaction', section: 'NBB', start: '2019-01-07 09:00', end: '2019-01-07 10:00' },
+  { code: 'DPS 924', name: 'Mobile App Development - Android', section: 'NSA', start: '2019-01-08 10:30', end: '2019-01-08 15:30' }
   // TODO: add more courses
 ]
