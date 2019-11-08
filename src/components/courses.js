79ecd25b66751a60daacf5c00381c3d3cc7e9db3
Jay Yin <jayyin11043@hotmail.com>
2019-11-07 21:48:44 -0500

    added more courses

--- src/components/courses.js
+++ src/components/courses.js
@@ -1,6 +1,8 @@
 export default [
   // TODO: add room & time for a course
   { code: 'BTH 745', name: 'Human-Computer Interaction', section: 'NBB', start: '2019-01-07 09:00', end: '2019-01-07 10:00', color: 'blue' },
-  { code: 'DPS 924', name: 'Mobile App Development - Android', section: 'NSA', start: '2019-01-08 10:30', end: '2019-01-08 15:30', color: 'red' }
+  { code: 'DPS 924', name: 'Mobile App Development - Android', section: 'NSA', start: '2019-01-08 10:30', end: '2019-01-08 15:30', color: 'red' },
+  { code: 'LSP 400', name: 'Presentations', section: 'NBD', start: '2019-01-10 15:40', end: '2019-01-10 17:30', color: 'green' },
+  { code: 'BTH 745', name: 'Human-Computer Interaction', section: 'NBB', start: '2019-01-11 12:00', end: '2019-01-11 15:00', color: 'blue' }
   // TODO: add more courses
 ]
